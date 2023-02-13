18989056c97d4efbe426d24c1c40d29028091826
nilee03 <125286761+nilee03@users.noreply.github.com>
2023-02-13 09:08:03 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let confrontButton = document.querySelector(".option-one");
+let buttonyes = document.querySelector(".buttonyes");
+
+let body = document.querySelector(".body");
+
+let confrontDiv = document.querySelector(".option-one-screen");
+let buildDiv = document.querySelector(".option-two-screen");
+
+
+
+
+let kidnappDiv = document.querySelector(".option-one-end");
+let savedDiv = document.querySelector(".option-two-end");
+
+// confrontButton.onclick=function(){
+// 	confrontDiv.style.display="block";
+// 	body.style="background-color:#05c5fa";
+// };
+
+buttonyes.onclick=function(){
+buildDiv.style.display="block";
+        body.style="background-color: lightpink";
+};
+
+
+// ohNoButton.onclick=function(){
+//  kidnappDiv.style.display="block";
+// };
+
+
